d8760ecefa60ea632ec00800a4bf531f65a405ea
wayne <molockree@gmail.com>
2017-08-22 14:34:27 +0800

    do practice for course 3

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour {
+
+	public void GoToScene(string sceneName)
+	{
+		Debug.Log("Go To Another Scene!");
+		SceneManager.LoadScene(sceneName);
+	}
+
+}
---
+++ Assets/VRND-Course3-Course_Assets/StarterContent/RotateLight.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateLight : MonoBehaviour {
+
+	public GameObject directionalLight;
+
+	float startTime = 0f;
+	bool isPressed = false;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		directionalLight.transform.Rotate(0f, Time.deltaTime, 0f);
+	}
+
+	public void ActivateRotation()
+	{
+
+	}
+}
